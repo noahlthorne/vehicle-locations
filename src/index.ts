0136e0047deedc49e1c6b5c8f4201697c07e42fa
noahlthorne <noahlthorne@gmail.com>
2021-05-09 15:34:10 +0900

    move index to src folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import http from 'http';
+import cors from 'cors';
 import { Server } from 'socket.io';
 
 const app = express();
@@ -8,12 +9,26 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(cors());
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when a client connects
 io.on('connection', (socket) => {
-    console.log('new connection...');
+    //Welcome current user
+    socket.emit('message', 'welcome to Chat Cord!');
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joied the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 });
 
 server.listen(PORT, () => {
