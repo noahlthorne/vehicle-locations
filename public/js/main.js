6f31e7b4e0acf8d2c77e4d32daf135931c32be40
noahlthorne <noahlthorne@gmail.com>
2021-05-09 15:34:44 +0900

    add messaging event listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
 const socket = io();
+const chatForm = document.getElementById('chat-form');
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+//Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get message text
+    const msg = e.target.elements.msg.value;
+
+    //Emit message to server
+    socket.emit('chatMessage', msg);
+});
